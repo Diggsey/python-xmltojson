50f57a8a07b0bfbbf1b85d32335dfede950ca328
Diggory Blake <diggsey@googlemail.com>
2019-02-11 18:00:28 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='xmltojson',
+    version='0.1',
+    py_modules=['xmltojson'],
+    license='MIT License',
+    long_description='Predictable XML to JSON conversion',
+)
